f949570a15257bb7b6c18210315d70e138122047
ponybai <pony.bai@gmail.com>
2023-01-25 21:21:32 +0800

    3: Forms and Events

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '../api/TasksCollection';
 import { Task } from './Task';
+import { TaskForm } from './TaskForm';
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, {sort: {createAt: -1}}).fetch());
 
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
+
+      <TaskForm />
+
 
       <ul>
         {tasks.map(task => <Task key={ task._id } task={task}/>)}
